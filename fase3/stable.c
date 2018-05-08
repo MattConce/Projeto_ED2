e3187503f8528a742a340e6b9a34f516f9e94bcb
MattConce <mattsantos.conce@gmail.com>
2018-05-08 18:05:31 -0300

    adicionei o stable_destroy

--- fase3/stable.c
+++ fase3/stable.c
@@ -1,18 +1,27 @@
 #include <stdlib.h>
 #include "stable.h"
 #include "stable_s.h"
+#include <ctype.h>
 
 SymbolTable stable_create() {
-  SymbolTable st = malloc(sizeof(SymbolTable));
-  st->n = 0;
-  st->m = 4; // capacidade inicial 4 seguindo exemplo do Sedgewick
-  st->hash_table = (Node**) malloc(st->m*sizeof(Node));
-  for (int i = 0; i < st->m; i++) {
-    st->hash_table[i] = (Node*) malloc(sizeof(Node));
+  SymbolTable table = malloc(sizeof(SymbolTable));
+  table->n = 0;
+  table->m = 4; // capacidade inicial 4 seguindo exemplo do Sedgewick
+  table->hash_table = (Node**) malloc(table->m*sizeof(Node));
+  for (int i = 0; i < table->m; i++) {
+    table->hash_table[i] = (Node*) malloc(sizeof(Node));
   }
-  return st;
+  return table;
 }
 
 int main() {
 
+}
+
+void stable_destroy(SymbolTable table) {
+  for (int i = 0; i < table->m; i++) {
+    free(table->hash_table[i]);
+  }
+  free(table->hash_table);
+  free(table);
 }
