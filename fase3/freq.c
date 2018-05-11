d01db30afb4d3f4d23b5b35ea03dab979a40edab
MattConce <mattsantos.conce@gmail.com>
2018-05-11 13:11:09 -0300

    inserindo valores na st, mas erro na referencia para as funções de stable.h

--- fase3/freq.c
+++ fase3/freq.c
@@ -16,17 +16,51 @@
 #define xval (x->val)
 
 
-static void put(SymbolTable table, char *key, EntryData val) {
-  InsertionResult res = stable_insert(table, key);
-  *(res.data) = val;
-}
+// static void put(SymbolTable table, char *key, EntryData val) {
+//   InsertionResult res = stable_insert(table, key);
+//   *(res.data) = val;
+// }
+//
+// static EntryData get(SymbolTable table, char *key) {
+//   EntryData *val = stable_find(table, key);
+//   return *val;
+// }
 
-static EntryData get(SymbolTable table, char *key) {
-  EntryData *val = stable_find(table, key);
-  return *val;
-}
+// void read_words(SymbolTable table) {
+//   char c;
+//   char curr_word[MAX];
+//   int is_word = FALSE;
+//   int i = 0;
+//   while(TRUE) {
+//     c = fgetc(stdin);
+//     if (isspace(c) && !is_word) {
+//       continue;
+//     }
+//     else if (isspace(c) || c == EOF) {
+//       char *key = curr_word;
+//       InsertionResult res = stable_insert(table, key);
+//       if (res.new == 0) {
+//         res.data->i = (res.data->i)++;
+//       }
+//       else {
+//         res.data->i = 1;
+//       }
+//       is_word = FALSE;
+//       strcpy(curr_word, "");
+//       i = 0;
+//       if (c == EOF) {
+//         break;
+//       }
+//     }
+//     else {
+//       curr_word[i++] = c;
+//       is_word = TRUE;
+//     }
+//   }
+// }
 
-void read_words() {
+int main() {
+  SymbolTable table = stable_create();
   char c;
   char curr_word[MAX];
   int is_word = FALSE;
@@ -37,7 +71,14 @@ void read_words() {
       continue;
     }
     else if (isspace(c) || c == EOF) {
-      puts(curr_word);
+      char *key = curr_word;
+      InsertionResult res = stable_insert(table, key);
+      if (res.new == 0) {
+        res.data->i = (res.data->i)++;
+      }
+      else {
+        res.data->i = 1;
+      }
       is_word = FALSE;
       strcpy(curr_word, "");
       i = 0;
@@ -50,13 +91,5 @@ void read_words() {
       is_word = TRUE;
     }
   }
-}
-
-int main() {
-  // char *words[MAX];
-  // int w = 0;
-  read_words();
-  puts("Done");
-  // printf("%d\n", w);
   return 1;
 }
