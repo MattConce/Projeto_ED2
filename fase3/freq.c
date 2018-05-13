dd077fc3baf3186f03d218b0eeeb16cf05b398d9
ikedawalk <arukimi@gmail.com>
2018-05-13 03:34:26 -0300

    Gambiarra final

--- fase3/freq.c
+++ fase3/freq.c
@@ -5,8 +5,10 @@
 #include "stable_s.h"
 #include "stable.h"
 
-static int compare (const void *s, const void *t) {
-  return strcmp (*(const char **) s, *(const char **) t);
+static int compare (const void* s, const void* t) {
+  const char* p = *(const char**)s;
+  const char* q = *(const char**)t;
+  return strcmp(p, q);
 }
 
 // funções visit não utilizadas nessa versão
@@ -30,7 +32,7 @@ int main(int argc, char const *argv[]) {
   FILE *read = fdopen(fd[0], "r");
 
   FILE *in = fopen(argv[1], "r");
-  char *word = (char*) malloc(2048 * sizeof(char));
+  char *word = (char*) malloc(1000 * sizeof(char));
   SymbolTable table = stable_create();
   // tamanho da maior palavra, alterado conforme lemos cada uma delas
   int greatest = 0;
@@ -47,23 +49,38 @@ int main(int argc, char const *argv[]) {
       // imprimir palavras novas no pipe
       fprintf(write, "%s ", word);
     }
-    word = (char*) malloc(2048 * sizeof(char)); // achar outro memadress para prox. word
+    word = (char*) malloc(1000 * sizeof(char)); // achar outro memadress para prox. word
   }
 
   fclose(write);
 
-  EntryData *ed;
-  char *entry = (char*) malloc(2048 * sizeof(char));
+  char *entry = (char*) malloc(1000 * sizeof(char));
+  char *s_array[table->n];
+  int index = 0;
 
-  // ler palavras do pipe
+  // ler palavras do pipe, colocar em um array de strings
   while (fscanf(read, "%s", entry) != EOF) {
+    s_array[index] = entry;
+    entry = (char*) malloc(1000 * sizeof(char));
+    index++;
+  }
+
+  // ordenar o array por qsort (stdlib.h)
+  qsort(s_array, table->n, sizeof(char*), compare);
+
+  // resetar index
+  index = 0;
+  EntryData *ed;
+
+  while (index < table->n) {
     // imprimir a palavra, os espacos necessarios, e o valor
-    printf("%s", entry);
-    for (int i = strlen(entry); i < greatest + 1; i++) {
+    printf("%s", s_array[index]);
+    for (int i = strlen(s_array[index]); i < greatest + 1; i++) {
       printf(" ");
     }
-    ed = stable_find(table, entry);
+    ed = stable_find(table, s_array[index]);
     printf("%d\n", ed->i);
+    index++;
   }
 
   //int test = stable_visit(table, print_key_int_spaced);
