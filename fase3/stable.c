aa73e1a7108cdce78dd98c685aa8f533645bf141
ikedawalk <arukimi@gmail.com>
2018-05-13 20:39:37 -0300

    delete me

--- fase3/stable.c
+++ fase3/stable.c
@@ -7,8 +7,6 @@
 #define xkey (x->key)
 #define xval (x->val)
 
-/******************************************************************************/
-
 static unsigned long hash(const char* key, int M) {
 
   unsigned long hash = 5381;
@@ -20,35 +18,6 @@ static unsigned long hash(const char* key, int M) {
 
   return hash;
 }
-
-static void rehash(SymbolTable table, const char *key, EntryData data) {
-
-    int i = hash(key, tablem);
-    Node *x = node_create(key);
-    xval = data;
-    node_insert(tableht[i], x);
-}
-
-static SymbolTable resize(SymbolTable table) {
-
-  SymbolTable newtbl = (SymbolTable) malloc(sizeof(SymbolTable));
-  newtbl->n = tablen;
-  int M = 2 * tablem;
-  newtbl->m = M;
-  newtbl->hash_table = (Node**) malloc(M * sizeof(Node*));
-
-  for (int i = 0; i < tablem; i++) {
-    Node *x = tableht[i];
-    while (x != NULL) {
-      rehash(newtbl, xkey, xval);
-      x = xnext;
-    }
-  }
-  free(table);
-  return newtbl;
-}
-
-/******************************************************************************/
 
 Node *node_create() {
 
@@ -78,7 +47,7 @@ SymbolTable stable_create() {
   SymbolTable table = (SymbolTable) malloc(sizeof(SymbolTable));
 
   tablen = 0;
-  tablem = 4;
+  tablem = 3200;
   tableht = (Node**) malloc(tablem * sizeof(Node*));
 
   for (int i = 0; i < tablem; i++) {
@@ -99,40 +68,27 @@ void stable_destroy(SymbolTable table) {
 
 InsertionResult stable_insert(SymbolTable table, const char *key) {
 
-  printf("Inserting key %s...\n", key);
-
   InsertionResult res;
-
-  if (tablen >= 10*tablem)
-    table = resize(table);
 
   unsigned long i = hash(key, tablem);
   Node *x = tableht[i];
 
   while (x != NULL) {
-    //printf("%lu\n",i);
-    // if (xnext != NULL)
-    //     printf("next key = %s\n", xnext->key);
     if (xkey != NULL && strcmp(xkey, key) == 0) {
-      // printf("i = %lu key = %s xkey = %s\n",i, key, xkey);
-      // printf("%d\n",xval.i);
       res.new = 0;
       res.data = &xval;
-      printf("Finished inserting, m is %d, n is %d\n", table->m, table->n);
       return res;
     }
     x = xnext;
   }
 
   x = node_create(key);
-  //printf("new node: i = %lu key = %s xkey = %s\n",i, key, xkey);
   node_insert(tableht[i], x);
   res.new = 1;
   res.data = &xval;
   x->key = malloc(sizeof(char*));
   strcpy(xkey, key);
   tablen++;
-  printf("Finished inserting, m is %d, n is %d\n", table->m, table->n);
   return res;
 }
 
@@ -158,7 +114,7 @@ int stable_visit(SymbolTable table, int (*visit)(const char *key, EntryData *dat
   for (int i = 0; i < tablem; i++) {
     Node *x = tableht[i]->next;
     while (x != NULL) {
-      bool = visit(xkey, &xval);
+      bool += visit(xkey, &xval);
       if (!bool) {
         return 0;
       }
